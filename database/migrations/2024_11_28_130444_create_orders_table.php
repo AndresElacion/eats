3f175c42f9379d8f5311c71f146c29696e15ed75
AndresElacion <andreselacioniii@gmail.com>
2024-11-30 14:18:38 +0800

    configured db

--- database/migrations/2024_11_28_130444_create_orders_table.php
+++ database/migrations/2024_11_28_130444_create_orders_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->decimal('total_price', 8, 2);
-            $table->enum('status', ['pending', 'confirmed', 'preparing', 'out_for_develiry', 'delivered', 'cancelled'])->default('pending');
-            $table->text('delivery_address');
-            $table->text('special_instruction')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('shop_id');
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('status', ['pending', 'confirmed', 'out_for_delivery', 'delivered', 'cancelled'])->default('pending');
+            $table->enum('payment_method', ['gcash', 'cash'])->default('gcash');
+            $table->enum('payment_status', ['pending', 'paid', 'failed'])->default('pending');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
             $table->timestamps();
         });
     }

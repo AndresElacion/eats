a0c4cb035f652ba9436e65dbe2138619134b6b5d
AndresElacion <andreselacioniii@gmail.com>
2024-11-30 14:16:33 +0800

    added cart

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\CartItem;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->composer('*', function ($view) {
+            $view->with('cartCount', CartItem::where('user_id', auth()->id())->sum('quantity'));
+        });
     }
 }
